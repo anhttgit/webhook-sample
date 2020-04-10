741a1d4b3f71c43e73c39c3edb61f675ee9f8123
anhttgit <50624328+anhttgit@users.noreply.github.com>
2020-04-11 00:12:24 +0700

    update app.js

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
-const express = require("express")
-const app = express()
+// # SimpleServer
+// A simple chat bot server
 
-// use the express-static middleware
-app.use(express.static("public"))
+var logger = require('morgan');
+var http = require('http');
+var bodyParser = require('body-parser');
+var express = require('express');
+var request = require('request');
+var router = express();
 
-// define the first route
-app.get("/webhook", function(req, res) {
-    res.send("<h1>Hello World!</h1>")
-})
+var app = express();
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+var server = http.createServer(app);
 
-// start the server listening for requests
-app.listen(process.env.PORT || 3000,
-    () => console.log("Server is running..."));
+
+app.get('/', (req, res) => {
+  res.send("Home page. Server running okay.");
+});
+
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.verify_token'] === 'anh_hoang_dep_trai_vo_doi') {
+    res.send(req.query['hub.challenge']);
+  }
+  res.send('Error, wrong validation token');
+});
+
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || process.env.IP || "127.0.0.1");
+
+server.listen(app.get('port'), app.get('ip'), function() {
+  console.log("Chat bot server listening at %s:%d ", app.get('ip'), app.get('port'));
+});
