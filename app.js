de7663b3a8ec7c67dfaaaeb823482295be255259
anhttgit <50624328+anhttgit@users.noreply.github.com>
2020-04-11 00:19:18 +0700

    Update app.js

--- app.js
+++ app.js
@@ -31,6 +31,4 @@ app.get('/webhook', function(req, res) {
 app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000);
 app.set('ip', process.env.OPENSHIFT_NODEJS_IP || process.env.IP || "127.0.0.1");
 
-server.listen(app.get('port'), app.get('ip'), function() {
-  console.log("Chat bot server listening at %s:%d ", app.get('ip'), app.get('port'));
-});
+server.listen(app.get('port'));
