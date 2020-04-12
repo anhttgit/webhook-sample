b83f831282f797a19dcf67263344de960edc5a9b
anhttgit <50624328+anhttgit@users.noreply.github.com>
2020-04-13 01:51:50 +0700

    Update app.js

--- app.js
+++ app.js
@@ -17,6 +17,14 @@ app.get("/webhook", function(req, res) {
     });
     res.send("<h1>Hello World!</h1>")
 })
+
+app.get("/log", function(req, res) {
+    fs.readFile('test.txt', function(err, data) {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        return res.end();
+    });
+})
 const userAction = async(myBody) => {
     const response = await fetch('http://localhost:3000/url', {
         method: 'POST',
