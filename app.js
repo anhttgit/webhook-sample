86257ef1bbb14a945016e316bb22ef160211b667
anhttgit <50624328+anhttgit@users.noreply.github.com>
2020-04-13 01:42:24 +0700

    Update app.js

--- app.js
+++ app.js
@@ -4,11 +4,17 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.text({ type: 'text/plain' }))
     // use the express-static middleware
 app.use(express.static("public"))
+const fs = require('fs');
 
 // define the first route
 app.get("/webhook", function(req, res) {
     userAction(req.body);
-
+    fs.writeFile("test.txt", req.body, function(err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("The file was saved!");
+    });
     res.send("<h1>Hello World!</h1>")
 })
 const userAction = async(myBody) => {
@@ -25,5 +31,5 @@ const userAction = async(myBody) => {
 
 
 // start the server listening for requests
-app.listen(process.env.PORT || 3000,
+app.listen(process.env.PORT || 3001,
     () => console.log("Server is running..."));
